0244ce670c16359ea32d0a33ad95e5c967fe888a
Cîrstoiu Bogdan-Florin <62263505+weilderbeast@users.noreply.github.com>
2021-05-06 16:13:40 +0300

    Added bounds for place markers

--- src/main/java/com/example/routeoramaserver/dao/places/IPlaceDAO.java
+++ src/main/java/com/example/routeoramaserver/dao/places/IPlaceDAO.java
@@ -3,8 +3,10 @@ package com.example.routeoramaserver.dao.places;
 import com.example.routeoramaserver.models.Place;
 
 import java.sql.SQLException;
+import java.util.List;
 
 public interface IPlaceDAO {
     Place NewPlace(Place place) throws SQLException;
     Place GetPlace(String place) throws SQLException;
+    List<Place> getPlacesInBounds(List<Double> bounds) throws SQLException;
 }
--- src/main/java/com/example/routeoramaserver/dao/places/PlaceDAOManager.java
+++ src/main/java/com/example/routeoramaserver/dao/places/PlaceDAOManager.java
@@ -8,6 +8,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PlaceDAOManager implements IPlaceDAO {
 
@@ -163,5 +165,58 @@ public class PlaceDAOManager implements IPlaceDAO {
             if (connection != null) try { connection.close(); } catch (Exception e) { e.printStackTrace(); }
         }
         return newLocation;
+    }
+
+    @Override
+    public List<Place> getPlacesInBounds(List<Double> bounds) {
+        double latSW = bounds.get(1), lngSW = bounds.get(0), latNE = bounds.get(3), lngNE = bounds.get(2);
+
+        Connection connection = null;
+        PreparedStatement statement = null;
+        ResultSet resultSet = null;
+        List<Place> sendBack = new ArrayList<>();
+
+        try {
+            connection = databaseConnection.getConnection();
+            connection.setSchema("Routeourama");
+            statement = connection.prepareStatement("SELECT * FROM \"Place\"" +
+                    "FULL OUTER JOIN \"Location\" ON \"Place\".placeid = \"Location\".placeid WHERE \"Location\".lat BETWEEN ? AND ?\n" +
+                    "AND \"Location\".lng BETWEEN ? AND ?");
+            statement.setDouble(1, latSW);
+            statement.setDouble(2, latNE);
+            statement.setDouble(3, lngSW);
+            statement.setDouble(4, lngNE);
+            resultSet = statement.executeQuery();
+
+            while(resultSet.next()){
+                int placeid = resultSet.getInt("placeid");
+                String placeName1 = resultSet.getString("name");
+                String description = resultSet.getString("description");
+                int followCount = resultSet.getInt("followCount");
+                int userid = resultSet.getInt("userid");
+                System.out.println("New place fetched");
+                Place newPlace = new Place(placeid, placeName1, description, followCount, userid);
+
+                double lat = resultSet.getDouble("lat");
+                double lng = resultSet.getDouble("lng");
+                String country = resultSet.getString("country");
+                String city = resultSet.getString("city");
+                System.out.println("New location fetched");
+
+                if(newPlace != null) {
+                    newPlace.setLocation(new Location(lat, lng, country, city));
+                    sendBack.add(newPlace);
+                }
+            }
+
+            return sendBack;
+        }
+        catch (SQLException ex) { System.out.println("Get location with specified credentials failed" + ex.getMessage()); }
+        finally {
+            if (resultSet != null) try { resultSet.close(); } catch (Exception e) { e.printStackTrace(); }
+            if (statement != null) try { statement.close(); } catch (Exception e) { e.printStackTrace(); }
+            if (connection != null) try { connection.close(); } catch (Exception e) { e.printStackTrace(); }
+        }
+        return sendBack;
     }
 }
